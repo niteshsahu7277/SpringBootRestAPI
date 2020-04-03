4dd0a1b4ba7ea80dd1ef50ca48aab06add9a36d9
nites <nites@DESKTOP-GQCD0HP>
2020-04-03 13:19:58 +0530

    After searhing,updating and deleting blog

--- src/main/java/com/niit/myblog/controller/BlogDataController.java
+++ src/main/java/com/niit/myblog/controller/BlogDataController.java
@@ -1,9 +1,15 @@
 package com.niit.myblog.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +20,42 @@ import com.niit.myblog.repository.BlogRepository;
 public class BlogDataController {
 	@Autowired
 	BlogRepository blogRepository;
+	
 	@PostMapping("/blogdata")
 	public Blog create(@RequestBody Map<String, String> body){
 	String title = body.get("title");
 	String content = body.get("content");
 	return blogRepository.save(new Blog(title,content));
+	}
+	
+	@GetMapping("/blogdata")
+	public List<Blog> index(){
+		return blogRepository.findAll();
+	}
+	@GetMapping("/blogdata/{id}")
+	public Optional<Blog> show(@PathVariable String id){
+	int blogId = Integer.parseInt(id);
+	return blogRepository.findById(blogId);
+	}
+	@PutMapping("/blogdata/{id}")
+	public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
+	int blogId = Integer.parseInt(id);
+	// getting blog
+	Blog blog = blogRepository.findById(blogId).get();
+	blog.setTitle(body.get("title"));
+	blog.setContent(body.get("content"));
+	return blogRepository.save(blog);
+	}
+	@PostMapping("/blogdata/search")
+	public List<Blog> search(@RequestBody Map<String, String> body){
+	String searchTerm = body.get("text");
+	return blogRepository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
+	}
+	@DeleteMapping("blogdata/{id}")
+	public boolean delete(@PathVariable String id){
+	int blogId = Integer.parseInt(id);
+	blogRepository.deleteById(blogId);
+	return true;
 	}
 
 }
--- src/main/java/com/niit/myblog/repository/BlogRepository.java
+++ src/main/java/com/niit/myblog/repository/BlogRepository.java
@@ -10,4 +10,8 @@ public interface BlogRepository extends JpaRepository<Blog, Integer> {
 	// custom query to search to blog post by title or content
 	List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
 
+
+
+
+
 }
